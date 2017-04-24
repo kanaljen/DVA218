8038a1db8b6c3e34e1abdde186ddfcea2df40036
Kanaljen <sbn14007@student.mdh.se>
2017-04-24 15:22:42 +0200

    Added files for
    Setup for lab3, new files for both server and client.

---
+++ lab3/server.h
@@ -0,0 +1,14 @@
+//
+//  server.h
+//  server
+//
+//  Created by Staffan Brickman on 2017-04-24.
+//  Copyright © 2017 Staffan Brickman. All rights reserved.
+//
+
+#ifndef server_h
+#define server_h
+
+#include <stdio.h>
+
+#endif /* server_h */
